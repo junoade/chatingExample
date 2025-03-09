e526209751b1dc81eb17ac48e67277de476b8ab1
ajchoi0928 <ajchoi0928@gmail.com>
2025-03-10 00:15:03 +0900

    feat: member 회원가입 및 로그인시 jwt 토큰 방식 적용
    1. 스프링 시큐리티 설정
    2. filter 설정
    3. 비즈니스 로직에 적용

--- src/main/java/com/example/chatserver/member/service/MemberService.java
+++ src/main/java/com/example/chatserver/member/service/MemberService.java
@@ -1,18 +1,26 @@
 package com.example.chatserver.member.service;
 
 import com.example.chatserver.member.domain.Member;
+import com.example.chatserver.member.dto.MemberLoginReqDto;
 import com.example.chatserver.member.dto.MemberSaveReqDto;
 import com.example.chatserver.member.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional // 일단 편하게 구현
+@Log4j2
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public MemberService(MemberRepository memberRepository) {
+    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
         this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Member create(MemberSaveReqDto memberSaveReqDto) {
@@ -23,12 +31,17 @@ public class MemberService {
         Member newMember = Member.builder()
                 .name(memberSaveReqDto.getName())
                 .email(memberSaveReqDto.getEmail())
-                .password(memberSaveReqDto.getPassword())
+                .password(passwordEncoder.encode(memberSaveReqDto.getPassword()))
                 .build();
-
         Member member = memberRepository.save(newMember); // 기본적으로 JPA 의 return 값은 해당 도메인 엔티티
-
         return member;
+    }
 
+    public Member login(MemberLoginReqDto memberLoginReqDto) {
+        Member member = memberRepository.findByEmail(memberLoginReqDto.getEmail()).orElseThrow(() -> new EntityNotFoundException("존재하지 않는 이메일입니다."));
+        if (!passwordEncoder.matches(memberLoginReqDto.getPassword(), member.getPassword())) {
+            throw new IllegalArgumentException("아이디가 미존재하거나 비밀번호가 일치하지 않습니다");
+        }
+        return member;
     }
 }
