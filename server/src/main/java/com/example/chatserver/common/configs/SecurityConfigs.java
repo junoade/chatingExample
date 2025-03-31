cf3a0c78b20d37fa70bf2c25bc1d62c662006938
junoade <ajchoi0928@gmail.com>
2025-03-31 22:37:07 +0900

    feat: 간단한 WebSocket 실습
    #1 WebSocketConfigurer 구현체 작성
    #2 WebSocket 프로토콜을 위한 CORS 설정
    #3 WebSocketHandler 구현 - 상속 및 Thread-safe 자료구조 이용

--- server/src/main/java/com/example/chatserver/common/configs/SecurityConfigs.java
+++ server/src/main/java/com/example/chatserver/common/configs/SecurityConfigs.java
@@ -32,7 +32,7 @@ public class SecurityConfigs {
                 .csrf(AbstractHttpConfigurer::disable) // csrf 비활성화 // 서비스 로직단에서 방어한다
                 .httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic
                 // 특정 url 패턴에 대해서는 Authentication 객체를 요구하지 않도록 예외 등록 (인증처리 제외)
-                .authorizeHttpRequests(a -> a.requestMatchers("/member/create", "/member/doLogin").permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(a -> a.requestMatchers("/member/create", "/member/doLogin", "/connect").permitAll().anyRequest().authenticated())
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션방식 대신 토큰 방식 사용
 
                 // 나머지 url 패턴에선 토큰 검증
