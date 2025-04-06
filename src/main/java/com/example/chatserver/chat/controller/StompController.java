e6cf978de795dbc6b139b58839006e3bb5890aa9
junoade <ajchoi0928@gmail.com>
2025-04-06 21:30:24 +0900

    fix: 컨트롤러 내 Stomp 핸들링 변경

--- src/main/java/com/example/chatserver/chat/controller/StompController.java
+++ src/main/java/com/example/chatserver/chat/controller/StompController.java
@@ -1,21 +1,26 @@
 package com.example.chatserver.chat.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.handler.annotation.*;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 @Slf4j
 public class StompController {
+    private final SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/{roomId}") // 클라이언트에서 특정 publish/roomid 형태로 메시지 발행시 MessageMapping로 메세지 라우팅
-    @SendTo("/topic/{roomId}") // 해당 roomId 에 메시지를 발행하며 구독중인 클라이언트에게 메시지 전송
+    @MessageMapping("/send")
+    // old : 클라이언트에서 특정 publish/roomid 형태로 메시지 발행시 MessageMapping로 메세지 라우팅
+    // new : 동적 바인딩 잘 안됨
     // @DestinationVariable : @MessageMapping 과 함께 쓰이며, 정의된 WebSocket Controller 내에서만 사용
-    public String sendMessage(@DestinationVariable Long roomId, @Payload String message) {
-        log.info("Sending message: {}", message);
-        return message;
+    public void sendMessage(@Header("roomId") Long roomId, @Payload String message) {
+        log.info("Room {} message: {}", roomId, message);
+        // @SendTo("/topic/{roomId}") // 해당 roomId 에 메시지를 발행하며 구독중인 클라이언트에게 메시지 전송
+        // 문자열 리터럴만 허용하고 동적경로 안된다고함
+        messagingTemplate.convertAndSend("/topic/" + roomId, message);
+
     }
 }
